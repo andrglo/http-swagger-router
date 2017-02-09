fff90e4f110db19f67a0c54ca347c171eda5af46
Andre Gloria <andre@ayk.com.br>
2017-02-09 13:36:19 -0200

    Add timeout error

--- src/errors.js
+++ src/errors.js
@@ -1,6 +1,7 @@
 const AssertionError = 'AssertionError';
+const TimeoutError = 'TimeoutError';
 
-const assertionSchema = {
+const errorSchema = {
   properties: {
     name: {
       type: 'string'
@@ -11,17 +12,35 @@ const assertionSchema = {
   }
 };
 
-module.exports = {
+const errors = {
   schemas: {
-    assertionSchema
+    errorSchema
   },
   assertion: [{
     name: AssertionError,
-    schema: assertionSchema,
+    schema: errorSchema,
     catch: [AssertionError],
     show: error => ({
       name: 'AssertionError',
       message: error.message
     })
-  }]
+  }],
+  timeout: [{
+    status: 408,
+    name: TimeoutError,
+    schema: errorSchema,
+    catch: [
+      error => error.code === 'timeout'
+    ],
+    show: error => ({
+      name: TimeoutError,
+      message: 'Tempo de espera esgotado',
+    })
+  }],
+  all: [
+    ...errors.assertion,
+    ...errors.timeout
+  ]
 };
+
+module.exports = errors;
